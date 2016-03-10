8cfb2d6f16231af8b70c3e2d34ac3b8e785497e9
Bicho04 <Bicho04@gmail.com>
2016-03-10 03:53:45 -0300

    first commit

---
+++ _script/functions.js
@@ -0,0 +1,18 @@
+// var navigation = responsiveNav(".header-menu",{
+//     label: "",
+// });
+
+
+$(function(){
+    $('.nav-toggle').addClass('fa fa-bars');
+
+    $(".pictures").responsiveSlides({
+        speed: 600,
+        nav: true,
+        prevText: "",
+        nextText: "",   
+        navContainer: ".slide-navs",
+        namespace: "slide"
+    });
+
+});
